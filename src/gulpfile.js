0e3b72067824a86f121a0f9bc729e3a6e95ead7e
Rafael <rafael.nascimento.aguiar@outlook.com>
2020-07-22 23:46:53 -0300

    add: styles and media queries

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer')
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass() {
   return gulp
